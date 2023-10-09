4a149f1e6f8b514b3fc1e1f2ea30725566d40c46
Xander <45044697+xwyckoff@users.noreply.github.com>
2023-10-09 16:38:23 -0400

    added base next.js files

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import Image from 'next/image'
+import { useState } from 'react'
+
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    </main>
+  )
+}
